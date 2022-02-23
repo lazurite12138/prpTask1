1270dd3a9eccd9c016fcb9e3b232fcb5b28cee3b
余承源 <chengyuanYu@sjtu.edu.cn>
2022-02-23 22:34:59 +0800

    搭建了基本框架

--- ui/YCYMainWindow.py
+++ ui/YCYMainWindow.py
@@ -8,9 +8,34 @@ class YCYMainWindow(QWidget):
         self.initUI()
 
     def initUI(self):
+        self.createHboxGroupBox()#下方为水平布局
+        mainLayout = QVBoxLayout()
 
+        mainLayout.addWidget(self.Table())  # 在主页面插入表格
+        mainLayout.addLayout(self.createHboxGroupBox())  # 再整体插入下方的水平布局
+        self.setLayout(mainLayout)
+
+        self.setGeometry(460, 230, 1000, 600)
+        self.setWindowTitle('DICOM')
+
+    def createHboxGroupBox(self):
         layout=QHBoxLayout()
-        table=QTableWidget(6,6)#创建表格
+        #搜索框
+        searchLineEdit=QLineEdit()
+        searchLineEdit.setPlaceholderText("Find text...")
+        #打开文件和关闭窗口
+        OpenBtn=QPushButton("File")
+        CloseBtn=QPushButton("Close")
+
+        layout.addWidget(searchLineEdit,2)
+        layout.addStretch(5)
+        layout.addWidget(OpenBtn,1)
+        layout.addWidget(CloseBtn,1)
+
+        return layout
+
+    def Table(self):
+        table=QTableWidget(100,6)#创建表格,这里后续要改为行数与文件数相同
         table.setFrameShape(QFrame.NoFrame)#无边框的表格
         table.horizontalHeader().setFixedHeight(50)#表头高度
         table.horizontalHeader().setSectionResizeMode(5,QHeaderView.Stretch)  # 设置第6列宽度自动调整，充满屏幕
@@ -22,9 +47,9 @@ class YCYMainWindow(QWidget):
         table.setSelectionBehavior(QAbstractItemView.SelectRows)  # 设置不可选择单个单元格，只可选择一行。
         table.verticalHeader().setVisible(False)
         table.setShowGrid(False)
+        return table
 
-        layout.addWidget(table)
-        self.setLayout(layout)
 
-        self.setGeometry(500,500,500,500)
-        self.setWindowTitle('DICOM')
+
+
+
